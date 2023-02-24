8adf8c6d0c8b49b609097e31967aa32de6124b63
PrangonGhose <prangonghose@gmail.com>
2023-02-24 06:17:59 +0600

    Add SQLs

--- schema.sql
+++ schema.sql
@@ -18,3 +18,28 @@ CREATE TABLE animals(
 -- add species column
 ALTER TABLE animals
 ADD species VARCHAR(250);
+
+-- Create new tables
+CREATE TABLE owners(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(250),
+	age INT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(250),
+	PRIMARY KEY(id)
+);
+
+-- Remove column from animals table
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add species_id column and set to foreign key
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id);
+
+-- Add owner_id column and set to foreign key
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id);
