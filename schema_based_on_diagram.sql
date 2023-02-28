28d6aa5b397223453c979ad438373abc026f68ff
AlejandraKeber <alejandrakeber@gmail.com>
2023-02-28 15:13:10 -0500

    Add between_histories_treatments and invoice_items tables and create indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -22,3 +22,32 @@ CREATE TABLE invoices (
 	medical_history_id INT REFERENCES medical_histories(id),
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE treatments (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	type VARCHAR(250),
+	name VARCHAR(250),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE between_histories_treatments (
+	medical_histories_id INT REFERENCES medical_histories(id),
+	treatments_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE invoice_items (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT REFERENCES invoices(id),
+	treatment_id INT REFERENCES treatments(id),
+	PRIMARY KEY (id)
+);
+
+CREATE INDEX ON medical_histories(patient_id);
+CREATE INDEX ON invoice_items(invoice_id);
+CREATE INDEX ON invoice_items(treatment_id);
+CREATE INDEX ON invoices(medical_history_id);
+CREATE INDEX ON between_histories_treatments(medical_histories_id);
+CREATE INDEX ON between_histories_treatments(treatments_id);
